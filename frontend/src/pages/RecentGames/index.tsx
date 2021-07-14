bd0396a8249fe46c53cee6fba63554ae6073e9a6
Guidotti-dll <dev.lucasguidotti@gmail.com>
2021-07-14 18:12:31 -0300

    feat: get types with axios in recentGames

--- frontend/src/pages/RecentGames/index.tsx
+++ frontend/src/pages/RecentGames/index.tsx
@@ -1,17 +1,20 @@
 import React, { useState, useEffect } from 'react'
 import { HiOutlineArrowRight } from 'react-icons/hi'
+import { toast } from 'react-toastify'
 import { Link } from 'react-router-dom'
 import { Header, BetCard, GamesContainer } from './styles'
-import { types } from '../../utils/types'
 import { Button } from '../../styles/Button'
 import { useSelector } from 'react-redux'
 import { AppStore } from '../../store'
 import { formatDate, formatMoney } from '../../utils/formatValue'
 import { BetState } from '../../store/ducks/Bets'
+import api from '../../services/api'
+import { Type } from '../../interfaceies/game'
 
 const RecentGames: React.FC = () => {
   const { myNets } = useSelector<AppStore, BetState>(state => state.Bets)
   const [filteredGames, setFilteredGames] = useState(myNets)
+  const [types, setTypes] = useState<Type[]>([])
   const [filter, setFilter] = useState('')
   useEffect(() => {
     if (filter) {
@@ -21,6 +24,16 @@ const RecentGames: React.FC = () => {
     }
   }, [filter])
 
+  useEffect(() => {
+    api
+      .get('/games.json')
+      .then(({ data }) => {
+        console.log(data.types)
+        setTypes(data.types)
+      })
+      .catch(error => toast.error(error.message))
+  }, [])
+
   return (
     <GamesContainer>
       <Header>
@@ -28,6 +41,7 @@ const RecentGames: React.FC = () => {
           <h1>Recent Games</h1>
           <div className='filter'>
             <span>Filters</span>
+            {types.length === 0 && <span>Erro ao carregar filtro</span>}
             {types.map(type => (
               <Button
                 onClick={() => {
