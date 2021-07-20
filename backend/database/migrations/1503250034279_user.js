c32f3bac8737e1300987fb8aa1b1ad75b58ae81f
Guidotti-dll <dev.lucasguidotti@gmail.com>
2021-07-20 13:47:47 -0300

    feat: add users migration

--- backend/database/migrations/1503250034279_user.js
+++ backend/database/migrations/1503250034279_user.js
@@ -7,9 +7,12 @@ class UserSchema extends Schema {
   up () {
     this.create('users', (table) => {
       table.increments()
-      table.string('username', 80).notNullable().unique()
+      table.string('name', 80).notNullable().unique()
       table.string('email', 254).notNullable().unique()
       table.string('password', 60).notNullable()
+      table.boolean('is_confirmed').defaultTo(false).notNullable()
+      table.string('token')
+      table.timestamp('token_created_at')
       table.timestamps()
     })
   }
