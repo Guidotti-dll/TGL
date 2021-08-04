25cf047cd7bfaf0720e99839c5badd6ca5ce3d38
Guidotti-dll <dev.lucasguidotti@gmail.com>
2021-08-04 19:00:31 -0300

    feat: ForgotPassword request

--- mobile/src/Pages/ForgotPassword/index.tsx
+++ mobile/src/Pages/ForgotPassword/index.tsx
@@ -3,11 +3,13 @@ import { yupResolver } from '@hookform/resolvers/yup'
 import { StackNavigationProp } from '@react-navigation/stack'
 import React from 'react'
 import { SubmitHandler, useForm, Controller } from 'react-hook-form'
+import { ToastAndroid } from 'react-native'
 
 import { AuthStackParamList } from '../../Routes/Auth'
 import AuthContainer from '../../components/AuthContainer'
 import Input from '../../components/Input'
 import { colors } from '../../constants/colors'
+import api from '../../services/api'
 import { EmailSchema } from '../../utils/schemas'
 import { Button, ButtonText, Container, Title } from '../SignIn/styles'
 import { Form } from './syles'
@@ -34,8 +36,18 @@ const ForgotPassword = ({ navigation }: Props) => {
     resolver: yupResolver(EmailSchema),
   })
 
-  const onSubmit: SubmitHandler<EmailProps> = data => {
-    alert(JSON.stringify(data))
+  const onSubmit: SubmitHandler<EmailProps> = async ({ email }) => {
+    try {
+      await api.post('/forgot-password', {
+        email,
+        redirect_url: 'http://localhost:3000/reset-password',
+      })
+
+      alert('Enviamos um link para o seu email para recuperar a sua senha!!')
+      navigation.goBack()
+    } catch (error) {
+      alert(error.response.data.error.message)
+    }
   }
 
   return (
